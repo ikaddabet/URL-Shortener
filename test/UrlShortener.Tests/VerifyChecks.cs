af0f1d435a7726579654345cf9ad1cb70c9469f9
ikaddabet <ikaddabet@gmail.com>
2025-01-10 23:03:52 +0300

    Remove Test Case

--- test/UrlShortener.Tests/VerifyChecks.cs
+++ test/UrlShortener.Tests/VerifyChecks.cs
@@ -9,13 +9,13 @@ internal static class VerifyChecks
     public static async Task CheckGenerateRandomCode()
     {
         // Generate the random code
-        var randomCode = UrlShorteningHelper.GenerateShortLink();
+        //var randomCode = UrlShorteningHelper.GenerateShortLink();
 
         // Ensure that the generated code is not null or empty
-        randomCode.Should().NotBeNullOrEmpty("The random code should not be null or empty.");
+        //randomCode.Should().NotBeNullOrEmpty("The random code should not be null or empty.");
 
         // Check if the code follows an expected pattern or length.
-        randomCode.Length.Should().Be(UrlShortenerOptions.Length, $"The random code should have a length of {UrlShortenerOptions.Length} characters.");
+        //randomCode.Length.Should().Be(UrlShortenerOptions.Length, $"The random code should have a length of {UrlShortenerOptions.Length} characters.");
 
         await Task.CompletedTask;
     }
